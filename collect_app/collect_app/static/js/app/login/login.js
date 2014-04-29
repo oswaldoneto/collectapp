070315112b62a51cc23f7e32906f7d0eb18fec88
oswaldo.neto <oswaldo.neto@gmail.com>
2014-04-28 22:58:02 -0300

    Login

--- collect_app/collect_app/static/js/app/login/login.js
+++ collect_app/collect_app/static/js/app/login/login.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-	$("#uic_login").button();
+
+	$("#uic_login").click(function(){
+		$("#uic_login_form").attr("action", "/login/");  
+		$("#uic_login_form").submit(); 
+	});
+
 });
